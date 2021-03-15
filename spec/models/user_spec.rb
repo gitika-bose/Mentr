bbca783bcc32c6cfafd854e90e492267e1f63b8b
Gitika Bose <gitikabose@dyn-129-236-224-163.dyn.columbia.edu>
2021-03-15 16:59:30 -0400

    rspec add user model test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'add new user' do
+    context 'add gitika.bose@gmail.com' do
+      it 'no users initially' do
+        @user = User.find_by_email("gitika.bose@gmail.com")
+        expect(@user).to eq nil 
+      end
+      it 'one user added to database' do
+        User.create(email:"gitika.bose@gmail.com", password: "yogurt5678")
+        @user = User.find_by_email("gitika.bose@gmail.com")
+        expect(@user.email).to eq "gitika.bose@gmail.com"
+      end
+    end
+  end
+end
