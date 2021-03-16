05f7881b45dd6040107b6f0c8bed8e27b82a7f47
Gitika Bose <gitikabose@dyn-129-236-224-163.dyn.columbia.edu>
2021-03-15 23:13:42 -0400

    coverage tests for user.rb complete

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe UsersController, type: :controller  do
     end
     context "user is signed in" do
       login_user
-      it "should redirect to profile page" do
+      it "should render profile page" do
         get :profile, params: {}
         expect(response).to have_http_status(200)
       end
