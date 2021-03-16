2f97bb2d38e3c2718851f194a113de7027e08002
Gitika Bose <gitikabose@dyn-129-236-224-163.dyn.columbia.edu>
2021-03-15 21:08:43 -0400

    coverage includes FactoryBot

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller  do
+  describe "GET index" do
+    it "assigns @teams" do
+    end
+
+    login_user
+
+    it "renders the index template" do
+    end
+  end
+end
