e36b19e74b82bfc55c2b94482c03b058dd68ba85
jcg2201 <jcg2201@columbia.edu>
2021-03-15 22:18:14 -0400

    rspec user controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller  do
-  describe "GET index" do
-    it "assigns @teams" do
+  describe "GET #profile" do
+    context "user is not signed in" do
+      it "should redirect to login" do
+        get :profile, params: {}
+        expect(response).to redirect_to('/users/sign_in')
+      end
     end
-
-    login_user
-
-    it "renders the index template" do
+    context "user is signed in" do
+      login_user
+      it "should redirect to profile page" do
+        get :profile, params: {}
+        expect(response).to have_http_status(200)
+      end
     end
   end
 end
