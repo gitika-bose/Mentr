e5bdd0f077c7d758e319fe08b10f266119237d06
Gitika Bose <gitikabose@dyn-129-236-224-255.dyn.columbia.edu>
2021-03-29 16:51:06 -0400

    mailer

--- spec/routing_spec.rb
+++ spec/routing_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "routes", :type => :routing do
   describe 'root path' do
-    it 'for the public' do
+    it 'direct to homepage' do
       expect(:get => "/").to route_to(
-          :controller => "users",
-          :action=>"profile",
+          :controller => "homepage",
+          :action=>"index",
         )
     end
   end
