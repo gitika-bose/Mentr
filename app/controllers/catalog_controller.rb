b7daa5d32365470ea48ed1feff243a7a24c8dab4
Jakwanul-Safin <js5142@columbia.edu>
2021-03-29 17:24:07 -0400

    Catalog only shows registered mentors

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -2,15 +2,24 @@ class CatalogController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @mentors = get_mentors
     if params[:search] != nil and params[:search] != ""
-      @users = User.search(params[:search]);
+      @mentors = @mentors.where("username LIKE ? ", "%#{params[:search]}%");
     else
-      @users = User.all;
+      @mentors = @mentors.all;
     end
   end
 
   def show
     id = params[:id] # retrieve movie ID from URI route
-    @user = User.find(id)
+    @user = get_mentors.find_by user_id: id #Mentor.select('users.id AS id, users.email as email, users.username as username').joins(:user).all.find_by id: id
   end
+
+  private
+  def get_mentors
+    Mentor
+      .select('users.id AS user_id, users.username AS username, users.email AS email')
+      .joins(:user);
+  end
+
 end
