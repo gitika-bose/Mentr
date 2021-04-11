abacb9078f517e642ceca7ef9477477a2027a223
Jakwanul-Safin <js5142@columbia.edu>
2021-04-11 15:27:53 -0400

    Change to schema head

--- db/schema.rb
+++ db/schema.rb
@@ -86,7 +86,6 @@ ActiveRecord::Schema.define(version: 2021_04_10_182207) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
     t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
