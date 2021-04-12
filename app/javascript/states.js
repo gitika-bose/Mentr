2ebe1e3feda75751674cab4dfe2ec100b07cc749
Jakwanul-Safin <js5142@columbia.edu>
2021-04-12 15:30:32 -0400

    new db hanges

--- app/javascript/states.js
+++ app/javascript/states.js
@@ -2,27 +2,15 @@ import $ from 'jquery'
 import 'select2/dist/css/select2.css'
 import 'select2'
 
-/*
-$(".js-source-states").select2({
-    tags: true
-});
-*/
-  
-setTimeout(function(){
-    $('.js-source-states').select2({
-        tags: true
-      })
+$('#form-tags').ready(function () {
+    $('#form-tags').select2({
+        closeOnSelect: false
+    })
 });
 
-/*
-$('.posts.index').ready(function () {
-    $('.js-source-states').select2({
-        tags: true
-      })
-});
 
-window.addEventListener('DOMContentLoaded', () => {
-  $('.js-source-states').select2({
-    tags: true
-  })
-})*/
+window.onload = function () {
+    $('#form-tags').select2({
+        closeOnSelect: false
+    })
+};
