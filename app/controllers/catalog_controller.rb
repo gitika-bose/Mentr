fd8713bf53d8fbff6b39bee77fe889d86c41dfd2
Jakwanul-Safin <js5142@columbia.edu>
2021-04-12 17:50:57 -0400

    Refactored code so all info is accessible through mentor

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -11,26 +11,12 @@ class CatalogController < ApplicationController
     else
       id = params[:id]
       if id == "-1"
-        @mentors = get_mentor
+        @mentors = Mentor.withUserInfo
       else
         @subject = Subject.find_by_id(id)
         @mentors = @subject.mentors
       end
     end
-  end
-
-  private
-  def get_mentor
-    Mentor
-      .select('users.id AS user_id,
-        users.username AS username,
-        users.email AS email,
-        profile,
-        linkedin AS professional,
-        website,
-        location,
-        company')
-      .joins(:user);
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,21 +3,7 @@ class UsersController < ApplicationController
 
   def show
     id = params[:id]
-    @mentor = Mentor.find_by_id(id)
-    @user = get_mentors.find_by user_id: id
+    @user = Mentor.find_by user_id: id
   end
-
-  private
-  def get_mentors
-    Mentor
-      .select('users.id AS user_id,
-        users.username AS username,
-        users.email AS email,
-        profile,
-        linkedin AS professional,
-        website,
-        location,
-        company')
-      .joins(:user);
-  end
+  
 end
