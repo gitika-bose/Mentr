a0eeb4e2c95a4dbd8dbfbe8ff9c5edb721cd204c
jcg2201 <jcg2201@columbia.edu>
2021-04-21 21:59:39 -0400

    production smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,11 +2,18 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # action-mailer
-  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = { from: 'no-reply@example.com' }
   config.action_mailer.default_url_options = { host: 'polar-forest-34288.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => Rails.application.credentials.dig(:smtp, :user),
+    :password             => Rails.application.credentials.dig(:smtp, :pass),
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
   # config.action_mailer.perform_caching = false
 
   # active-storage
